4f016866c6ba681eefb5276738fddbbe06de5f4d
vernacular-liberty <112956848+vernacular-liberty@users.noreply.github.com>
2022-11-30 21:44:36 -0500

    Add files via upload

---
+++ Bag.java
@@ -0,0 +1,11 @@
+//imports
+import java.util.ArrayList;
+
+public class Bag {
+    List <String> inventory = new ArrayList<>(5);
+
+    public Bag(){
+        this.inventory = i;
+    }
+
+}
---
+++ Character.java
@@ -0,0 +1,34 @@
+public class Character {
+    String name;
+    double size;
+    final double originalSize;
+    int locationC;
+    int locationR;
+    Boolean canFly;
+    Boolean canWalk;
+    int health;
+    Boolean hasPill;
+    Boolean hasFungi;
+
+     /**
+     * Constructs a wonderland character.
+     * @param name string that stores the characters name.
+     * @param size the height of the character double in inches.
+     * @param oS the immutible original height of the character double in inches.
+     * @param locationC the location of the character column on the map.    
+     * @param locationR the location of the character row on the map.
+     */
+    public Character(String n, double s, double oS, int locationC, int locationR){
+        this.name = n;
+        this.size = s;
+        this.originalSize = oS;
+        this.locationC = locationC;
+        this.locationR = locationR;
+        this.canFly = false;
+        this.canWalk = true;
+        this.health = 100;
+        this.hasPill = false;
+        this.hasFungi = false;
+
+    }
+}
