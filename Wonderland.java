88e4e0df10928f6533b868bb08c98088e26da214
lcgarner <113074918+lcgarner@users.noreply.github.com>
2022-12-13 07:28:54 -0500

    Add files via upload

--- Wonderland.java
+++ Wonderland.java
@@ -6,7 +6,7 @@ public class Wonderland{
     //private Croquet croquet;
     private FungiForest fungiForest;
     private RabbitHole rabbitHole;
-    private TugleyWoods tugleyWoods;
+    private TulgeyWoods tulgeyWoods;
     private Boolean changeLocation;
     Character character;
 
@@ -14,13 +14,13 @@ public class Wonderland{
     public Wonderland(Character character){
         this.map = new String[][]{{"You have reached the edge of Wonderland. The sky and sea melt together into one grey plain.", "You walk along the beach to find a bottle of pills washed up on the shore.", "You have reached the light house at the endless sea... ", "After a long treck throught the mountains you have reached the top of Mont Jubjub! You can see anything from here", "The far castle!"}, 
         {"You have entered the croquet grounds. An intense game is taking place between Tweedledee and Tweedledum.","You have reached the grandest Castle in all the land, the castle of the queen of hearts.", "Court House", "You have reached the royal rose gardens.", "You are at the base of the mountains of division."}, 
-        {"You have reached the Mad Hatters house. It appear there is a tea party in session!", "You have reached the duchesses manner.", "You emerge from the woods into a meadow surrounding a small gabled house. You enter the house on the table is a container of pills.", "You enter a grove of massive mushrooms. Your eyes meet those of caterpillar who ignores you and takes a drag of a long hookah.", "Chilled to the bone you hurry on through the woods in the hopes of finding shelter."}, 
+        {"You have reached the Mad Hatters house. It appear there is a tea party in session!", "You have reached the duchesses manner.", "You emerge from the woods into a meadow surrounding a small gabled house. You enter the house, and on the table is a container of pills.", "You enter a grove of massive mushrooms. Your eyes meet those of a caterpillar who ignores you, taking a drag of a long hookah.", "Chilled to the bone, you hurry on through the woods in the hopes of finding shelter."}, 
         {"Welcome to the dismal mire...","You found a river. Should you drink the water? It could be dangerous...Strange things happen in Wonderland...", "Giant dragonflies swarm through the sky over your head squabling over potential perches.","The trees around you begin to morph into oversized mushrooms. You are pleased pick some fungi and arrange them into a bouquet. Your stomache growls...", "You sense someone is watching you in the darkness! Move faster"}, 
-        {"You cross the water to find a small door.","You reach the edge of a calm pond. Thirsty, you cup the water to your lips only to spit out a mouthful of tears.","","The forest thicken. A sign points east saying, 'Enter at your own peril!'", "You wander down another one of the Tulgey woods winding purple roads. Will you ever escape?"}};
+        {"You cross the water to find a small door.","You reach the edge of a calm pond. Thirsty, you cup the water to your lips only to spit out a mouthful of tears.","","The forest thickens. A sign points east saying, 'Enter at your own peril!'", "You wander down another one of the Tulgey woods winding purple roads. Will you ever escape?"}};
         
         this.character = character;
-        this.fungiForest = new FungiForest();
-        this.tugleyWoods = new TugleyWoods();
+        this.fungiForest = new FungiForest(this.character);
+        this.tulgeyWoods = new TulgeyWoods(this.character);
         this.rabbitHole = new RabbitHole(this.character);
         this.changeLocation = true;
 
@@ -173,7 +173,7 @@ public class Wonderland{
             System.out.println("Caterpillar Conversation");
 
         } if (this.map[this.character.locationC][this.character.locationR].contains("Tugley woods")){
-            this.tugleyWoods.play();
+            this.tulgeyWoods.play();
             System.out.println("Tugley Woods");
 
         } if (this.map[this.character.locationC][this.character.locationR].contains("gabeled")){
@@ -185,7 +185,7 @@ public class Wonderland{
         || this.map[this.character.locationC][this.character.locationR].contains("water") || this.map[this.character.locationC][this.character.locationR].contains("door") 
         || this.map[this.character.locationC][this.character.locationR].contains("croquet") || this.map[this.character.locationC][this.character.locationR].contains("caterpillar"))){
           
-            System.out.println("Note: If you grab an item you will loose the ability to fly.");
+            System.out.println("Note: If you grab an item you will lose the ability to fly.");
     
         }
     }
