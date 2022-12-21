3253fd8d6a759958589257721a310e6ec68d2acc
lcgarner <113074918+lcgarner@users.noreply.github.com>
2022-12-21 15:45:34 -0500

    Add files via upload

--- CheshireCat.java
+++ CheshireCat.java
@@ -12,6 +12,10 @@ public class CheshireCat{
             hint+="The CATERPILLAR is indeed very knowledgeable, but the longer you stay here, the blacker your lungs will get. Try not to talk about yourself too much; it really gets him going. And don't forget--you can't leave without a proper \"goodbye\"! For some reason, the CATERPILLAR is very particular about this sort of thing.";
         }
 
+        if(key == "Tulgey"){
+            hint+="Fun fact, I am pretty is well-known around here. Even the Queen is scared of me!";
+        }
+
         //It is possible to add whatever hint you want: just call System.out.println(cat.hint(key_word)) in the appropriate place in your code and add a corresponding if-statement here
         //FOR EXAMPLE:
             // if(key == "gabeled"){
