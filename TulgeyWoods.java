104423bb6963d1ee416d45b98f6375af5ac10ef3
lcgarner <113074918+lcgarner@users.noreply.github.com>
2022-12-21 15:42:50 -0500

    Add files via upload

--- TulgeyWoods.java
+++ TulgeyWoods.java
@@ -1,75 +1,75 @@
 //imports
-// import java.util.ArrayList;
+//import java.util.ArrayList;
 //import java.util.Random;
 //import java.util.Scanner;
 
 //Introduces Cheshire Cat
 public class TulgeyWoods{
-    // ArrayList<String> areas;
-    int index;
+    //ArrayList<String> areas;
+    //int index;
     Character character;
-    // String lantern;
-    Boolean inForest;
-    Boolean newArea;
-    // String stick;
+    CheshireCat cat;
+    //Boolean hasLantern;
+    //Boolean inForest;
+    //Boolean newArea;
+    //String hasStick;
 
     /**
      * Constructs Tulgey Woods.
-     * @param character the character playing the game
+     * @param character the users character.
      */
-    public TulgeyWoods(Character character){
-        this.index = 0;
+    public TulgeyWoods(Character character, CheshireCat cat){
+        //this.index = 0;
         this.character = character;
-        // this.lantern = "lantern";
-        // this.stick = "stick";
-
-        // areas.add("After hours of wandering, you happen upon an abandoned shed in a small clearing.");
-        // areas.add("After being without a path for hours, you wind up at a crossroads.");
-        // areas.add("You decide to take the pink path. ");
-        // areas.add("You decide to take the green path.");
-
+        this.cat = cat;
+        //this.hasLantern = false;
+        //this.hasStick = false;
+        //ArrayList<String> floors = new ArrayList<String>();
+        //floors.add("You strip over a large stick.");
     }
 
     /**
-    * Executes the conversation
-    * @param arguments
+    * Prints the conversation between Cheshire Cat and player
     */
     public void play(){
-        System.out.println("After hours of wandering, you wind up at a crossroads.");
-        System.out.println("Suddenly, you hear laughter coming from above. A striped cat appears in front of you, seemingly out of thin air.");
-        System.out.println("This is the Cheshire Cat. He grins from ear to ear, and he speaks in a sing-song voice.");
-        System.out.println("[FUN FACT: The Cheshire Cat is well-known around here. Even the Queen is scared of him!]");
-        System.out.println("Maybe the Cheshire Cat knows the way out of here!");
-        System.out.println("    \"What have I found here?\", asks the Cheshire Cat.");
-        System.out.println("    \"Nobody. Do you know which way I should go?\", you respond.");
-        System.out.println("    \"That depends on where you want to get to.\", he says.");
-        System.out.println("    \"It really doesn't matter.\"");
-        System.out.println("    \"Then I guess it doesn't matter which way you go!\", exclaims the Cheshire Cat.");
 
-        System.out.print("You realize that the Cheshire Cat is right: It doesn't matter. You wander whichever way you please until you find the end of the woods.");
+        System.out.println();
+        this.character.printSlow("After hours of wandering, you wind up at a crossroads.");
+        this.character.printSlow("Suddenly, you hear laughter coming from above.");
+        this.character.printSlow(cat.hint("Tulgey"));
+        //this.character.printSlow("A striped cat appears in front of you, seemingly out of thin air.");
+        this.character.printSlow("This is the Cheshire Cat. He grins from ear to ear, and he speaks in a sing-song voice.");
+        this.character.printSlow("Maybe the Cheshire Cat knows the way out of here!");
+        this.character.printSlow("    \"What have I found here?\", asks the Cheshire Cat.");
+        this.character.printSlow("    \"Nobody. Do you know which way I should go?\", you respond.");
+        this.character.printSlow("    \"That depends on where you want to get to.\", he says.");
+        this.character.printSlow("    \"It really doesn't matter.\"");
+        this.character.printSlow("    \"Then I guess it doesn't matter which way you go!\", exclaims the Cheshire Cat.");
+
+        this.character.printSlow("You realize that the Cheshire Cat is right: It doesn't matter. You wander whichever way you please until you find the end of the woods.");
     }
 
-        //System.out.println("Type \"enter\" if you want to enter the forest.");
+        //System.out.println("Type \"green\" if you want to take the green path; type \"pink\" if you want to take the pink path.");
 
-    //     String start = this.character.command();
-    //     userAction(start);
+        // String start = this.character.command();
+        // userAction(start);
 
-    //     String command;
+        // String command;
 
-    //     if (start.contains("enter")){
+        // if (start.contains("enter")){
 
-    //         while (this.inForest){
+        //     while (this.inForest){
 
-    //             if (this.newArea == true){
-    //                 this.newArea = false;
+        //         if (this.newArea == true){
+        //             this.newArea = false;
 
-    //                 System.out.println(this.areas.get(this.index));
-    //             }
-    //             command = this.character.command();
-    //             userAction(command);
-    //         }
-    //         System.out.println("Where did the Cheshire Cat come from? And why is his smile so wide?");
-    //     }
+        //             System.out.println(this.areas.get(this.index));
+        //         }
+        //         command = this.character.command();
+        //         userAction(command);
+        //     }
+        //     System.out.println("Where did the Cheshire Cat come from? And why is his smile so wide?");
+        // }
 
     // }
 
@@ -77,9 +77,40 @@ public class TulgeyWoods{
 
     // }
 
+    // public boolean hasLantern(){
+    //     return this.hasLantern;
+    // }
+
+    // public void userAction(String command){
+    //     if (command.contains("investigate") || command.contains("check out") || command.contains("go to")){
+    //         actionInvestigate(command);
+
+    //     } else if (command.contains("take") || command.contains("grab")){
+    //         actionTake(command);
+
+    //     } else if (command.contains("sleep")){
+    //         this.character.actionSleep();
+
+    //     } else if (command.contains("drop")){
+    //         this.character.drop(command);
+            
+    //     } else if (command.contains("clue")){
+    //         printCommand();
+    //     }else {
+    //         this.character.printSlow("I don't know that command yet.");
+    //     }
+    // }
+
+    // public void actionInvestigate(String command){
+    //     if (this.area.contains("stick" & this.command.contains("stick")){
+
+    //     }
+    // }
+
     public static void main(String[] args){
         Character a = new Character( 1, 1);
-        TulgeyWoods t = new TulgeyWoods(a);
+        CheshireCat c = new CheshireCat();
+        TulgeyWoods t = new TulgeyWoods(a, c);
         t.play();
     }
 }
